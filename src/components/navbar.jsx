be02aaf52793242d8dbfd1f1666dd10f031a2ee4
Jacob Foote <jacobjfoote01@gmail.com>
2023-11-21 15:37:50 -0700

    finished most of the functionality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,33 @@
-import { Container, Navbar, Nav } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function NavBar() {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const logOut = async () => {
-    const res = await axios.get('/server/logout')
+    const res = await axios.get("/server/logout");
     if (res.data.success) {
-      navigate('/')
+      navigate("/");
     } else {
-      alert('You must be logged in to Log Out')
+      alert("You must be logged in to Log Out");
     }
-  } 
+  };
 
   return (
     <>
-    <Navbar expand='lg'>
-      <Container>
-        <Navbar.Brand href="/app"  >Vehicle Log</Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end">
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Vehicle Log</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
             <Nav className="me-auto">
-                <Nav.Link href='/'>Login</Nav.Link>
-                <br/>
-                <Nav.Link onClick={logOut}>Log Out</Nav.Link>
+              <Nav.Link href="/">Login</Nav.Link>
+              <br />
+              <Nav.Link onClick={logOut}>Log Out</Nav.Link>
             </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
-
-
